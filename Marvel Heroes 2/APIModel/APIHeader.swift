5b87d1a743b2bd3bb9f80c41867daedade71e03f
bgilherr <jensenn.gamedonkey@gmail.com>
2018-12-05 07:48:27 +0100

    Commit

---
+++ Marvel Heroes 2/APIModel/APIHeader.swift
@@ -0,0 +1,9 @@
+//
+//  APIHeader.swift
+//  Marvel Heroes 2
+//
+//  Created by Borja Gil Herrero on 04/12/2018.
+//  Copyright © 2018 Borja Gil Herrero. All rights reserved.
+//
+
+import Foundation
